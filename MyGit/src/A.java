0150da7d3779376a25c95bd93337d41a7057a5a5
Kisoo <Kisoo@DESKTOP-CUI2F6B>
2020-06-16 12:18:19 +0900

    2nd commit

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -2,5 +2,6 @@
 public class A {
 	public static void main(String args[]) {
 		System.out.println("A: My Git!");
+		System.out.println("2nd version!");
 	}
 }
--- MyGit/src/C.java
+++ MyGit/src/C.java
@@ -2,5 +2,6 @@
 public class C {
 	public static void main(String args[]) {
 		System.out.println("C: My Git!");
+		System.out.println("2nd version!");
 	}
 }
